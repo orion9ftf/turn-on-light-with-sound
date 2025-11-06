d2029a182a00b4d857942a6a88f575a1fb67e14b
Maypher92 <eva.matus@hotmail.com>
2025-11-05 23:06:47 -0300

    turn or light with sound

---
+++ turn-on-light-with-sound.ino
@@ -0,0 +1,26 @@
+int luz = 12;
+int sensorSonido = 2;
+int sonido = 0;
+int var = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensorSonido, INPUT);
+  pinMode(luz, OUTPUT);
+}
+
+void loop() {
+  var  = digitalRead (sensorSonido);
+  if (var == HIGH) {
+    Serial.println ("encendido");
+    digitalWrite (luz, HIGH) ;
+    sonido = sonido+1;
+    delay (90);
+  }
+
+  if (sonido == 2) {
+    Serial.println ("apagado"); 
+    digitalWrite (Luz, LOW);
+    sonido = 0;
+  }
+}
